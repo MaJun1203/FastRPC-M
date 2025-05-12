76fe9112410f12ac6626d9d99be1833c102c75ce
Ma Jun <14615397+ma-jun-1203@user.noreply.gitee.com>
2025-05-12 12:50:36 +0800

    添加Vert.x TCP客户端和服务器实现，支持基本的连接和请求处理逻辑

---
+++ ma-rpc-core/src/main/java/com/xiaoma/marpc/serializer/ProtocolMessageSerializerEnum.java
@@ -0,0 +1,76 @@
+package com.xiaoma.marpc.serializer;
+
+import cn.hutool.core.util.ObjectUtil;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Class Name: ProtocolMessageSerializerEnum
+ * Description:协议消息的序列化器枚举
+ * Created on: 2025/5/12
+ * Author: xiaoma
+ */
+
+@Getter
+public enum ProtocolMessageSerializerEnum {
+
+    JDK(0, "jdk"),
+    JSON(1, "json"),
+    KRYO(2, "kryo"),
+    HESSIAN(3, "hessian");
+
+    private final int key;
+
+    private final String value;
+
+    ProtocolMessageSerializerEnum(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    /**
+     * 获取值列表
+     *
+     * @return
+     */
+    public static List<String> getValues() {
+        return Arrays.stream(values()).map(item -> item.value).collect(Collectors.toList());
+    }
+
+    /**
+     * 根据 key 获取枚举
+     *
+     * @param key
+     * @return
+     */
+    public static ProtocolMessageSerializerEnum getEnumByKey(int key) {
+        for (ProtocolMessageSerializerEnum anEnum : ProtocolMessageSerializerEnum.values()) {
+            if (anEnum.key == key) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 根据 value 获取枚举
+     *
+     * @param value
+     * @return
+     */
+    public static ProtocolMessageSerializerEnum getEnumByValue(String value) {
+        if (ObjectUtil.isEmpty(value)) {
+            return null;
+        }
+        for (ProtocolMessageSerializerEnum anEnum : ProtocolMessageSerializerEnum.values()) {
+            if (anEnum.value.equals(value)) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+}
