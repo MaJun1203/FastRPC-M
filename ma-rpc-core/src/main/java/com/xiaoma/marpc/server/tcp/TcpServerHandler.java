abf405fe42ca94d364138761af50c68f53914492
Ma Jun <14615397+ma-jun-1203@user.noreply.gitee.com>
2025-05-12 22:26:04 +0800

    添加TcpBufferHandlerWrapper类以增强粘包半包处理能力，重构TcpServerHandler和VertxTcpClient以使用新的处理逻辑

--- ma-rpc-core/src/main/java/com/xiaoma/marpc/server/tcp/TcpServerHandler.java
+++ ma-rpc-core/src/main/java/com/xiaoma/marpc/server/tcp/TcpServerHandler.java
@@ -24,45 +24,10 @@ import java.lang.reflect.Method;
 
 public class TcpServerHandler implements Handler<NetSocket> {
     @Override
-    public void handle(NetSocket netSocket) {
-        // 处理连接
-        netSocket.handler(buffer -> {
-            ProtocolMessage<RpcRequest> protocolMessage;
-            try {
-                protocolMessage = (ProtocolMessage<RpcRequest>) ProtocolMessageDecoder.decode(buffer);
-            } catch (IOException e) {
-                throw new RuntimeException("协议消息解码错误");
-            }
-            RpcRequest rpcRequest = protocolMessage.getBody();
-
-            // 处理请求
-            // 构造响应结果对象
-            RpcResponse rpcResponse = new RpcResponse();
-            try{
-                Class<?> implClass = LocalRegistry.get(rpcRequest.getServiceName());
-                Method method = implClass.getMethod(rpcRequest.getMethodName(), rpcRequest.getParameterTypes());
-                Object result = method.invoke(implClass.newInstance(), rpcRequest.getParameters());
-                rpcResponse.setData(result);
-                rpcResponse.setDataType(method.getReturnType());
-                rpcResponse.setMessage("ok");
-            } catch (Exception e) {
-                e.printStackTrace();
-                rpcResponse.setMessage(e.getMessage());
-                rpcResponse.setException(e);
-            }
-            // 发送响应，编码
-            ProtocolMessage.Header header = protocolMessage.getHeader();
-            header.setType((byte) ProtocolMessageTypeEnum.RESPONSE.getKey());
-            ProtocolMessage<RpcResponse> responseProtocolMessage = new ProtocolMessage<>(header, rpcResponse);
-            try {
-                Buffer encode = ProtocolMessageEncoder.encode(responseProtocolMessage);
-                netSocket.write(encode);
-            } catch (IOException e) {
-                throw new RuntimeException("协议消息编码错误");
-            }
-
+    public void handle(NetSocket socket) {
+        TcpBufferHandlerWrapper bufferHandlerWrapper = new TcpBufferHandlerWrapper(buffer -> {
+            // 处理请求代码
         });
-
-
+        socket.handler(bufferHandlerWrapper);
     }
 }
--- ma-rpc-core/src/main/java/com/xiaoma/marpc/server/tcp/VertxTcpServer.java
+++ ma-rpc-core/src/main/java/com/xiaoma/marpc/server/tcp/VertxTcpServer.java
@@ -1,9 +1,11 @@
 package com.xiaoma.marpc.server.tcp;
 
 import com.xiaoma.marpc.server.HttpServer;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServer;
+import io.vertx.core.parsetools.RecordParser;
 
 /**
  * Class Name: VertxTcpServer
@@ -20,7 +22,37 @@ public class VertxTcpServer implements HttpServer {
         // 创建 TCP 服务器
         NetServer server = vertx.createNetServer();
         // 处理请求
-        server.connectHandler(new TcpServerHandler());
+        server.connectHandler(socket -> {
+            // 构造 parser
+            RecordParser parser = RecordParser.newFixed(8);
+            parser.setOutput(new Handler<Buffer>() {
+                // 初始化
+                int size = -1;
+                // 一次完整的读取（头 + 体）
+                Buffer resultBuffer = Buffer.buffer();
+
+                @Override
+                public void handle(Buffer buffer) {
+                    if (-1 == size) {
+                        // 读取消息体长度
+                        size = buffer.getInt(4);
+                        parser.fixedSizeMode(size);
+                        // 写入头信息到结果
+                        resultBuffer.appendBuffer(buffer);
+                    } else {
+                        // 写入体信息到结果
+                        resultBuffer.appendBuffer(buffer);
+                        System.out.println(resultBuffer.toString());
+                        // 重置一轮
+                        parser.fixedSizeMode(8);
+                        size = -1;
+                        resultBuffer = Buffer.buffer();
+                    }
+                }
+            });
+
+            socket.handler(parser);
+        });
 
         // 启动 TCP 服务器并监听指定端口
         server.listen(port, result -> {
